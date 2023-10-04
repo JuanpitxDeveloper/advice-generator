ec45d7799c411c68492b37e2e8761d40cbf0458f
JuanpitxDeveloper <juanchojm0161@gmail.com>
2023-10-03 20:03:24 -0400

    Proyecto *Finalizado

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  const [advice, setAdvice] = useState ({});
+  const [slipId, setSlipId] = useState (1);
+
+    useEffect(() => {
+      fetch ("https://api.adviceslip.com/advice")
+      .then (response => response.json())
+      .then (data => {
+        setAdvice(data.slip);
+        setSlipId(data.slip.id);
+      });
+    }, []);
+
+    const handleButtonClick = () => {
+      fetch ("https://api.adviceslip.com/advice")
+    .then (response => response.json())
+    .then (data => {
+      setAdvice(data.slip);
+      setSlipId(data.slip.id);
+    });
+    }
+    return (
+      <main className='bg-dark w-screen h-screen flex items-center justify-center conten
+      t-center '>
+      <div className='bg-grey flex flex-col  justify-center items-center text-center rounded-xl   text-skyBlue md:w-[555px] md:h-[330px] w-[344px] h-[425px]'>
+        <h1 className="text-[14px] font-bold text-neon  md:pb-[12px]">
+          A  D  V  I  C  E  #  {slipId}
+        </h1>
+        
+        <div className='text-[28px]  md:w-[520px] md:h-[180px] flex text-center justify-center items-center md:px-[10px] w-[320px] h-[286px] mx-8 '>
+          <p>“{advice.advice}”</p>
+        </div>
+    
+        <div className='md:hidden'>
+          <img src="pattern-divider-mobile.svg" alt="" />
+        </div>
+    
+        <div className='hidden md:block '>
+          <img src="pattern-divider-desktop.svg" alt="" />
+        </div>
+      </div>
+    
+      <button onClick={handleButtonClick} className='absolute bottom-[90px] bg-neon rounded-full flex items-center h-[65px] w-[65px] justify-center md:bottom-[255px] hover:shadow-[0px_0px_40px_0px_#52ffa8]'>
+        <img src="icon-dice.svg" alt="" />
+      </button>
+    </main>
+    
 )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      skyBlue:'hsl(193, 38%, 86%)',
+      darkBlue:'hsl(217, 19%, 38%)',
+      grey:'hsl(217, 19%, 24%)',
+      dark:'hsl(218, 23%, 16%)',
+      neon:'hsl(150, 100%, 66%)'
+    },
     extend: {},
   },
   plugins: [],
